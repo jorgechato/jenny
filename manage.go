36463e38a5ebdccfbc6b6d0333fadaf3137642f5
orggue <jorgechato1@gmail.com>
2017-10-04 18:12:22 +0200

    UPDATE: demo

--- manage.go
+++ manage.go
@@ -11,6 +11,22 @@ var (
 	revision string
 )
 
+// executor executes command and print the output.
+func executor(in string) {
+	fmt.Println("Your input: " + in)
+}
+
+// completer returns the completion items from user input.
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "users", Description: "user table"},
+		{Text: "sites", Description: "sites table"},
+		{Text: "articles", Description: "articles table"},
+		{Text: "comments", Description: "comments table"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
 func main() {
 	fmt.Printf("Jenny %s (rev-%s)\n", version, revision)
 	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program..")
@@ -24,8 +40,8 @@ func main() {
 
 	fmt.Println(j.PasswordMatch("sd"))
 	p := prompt.New(
-		//kube.Executor,
-		//kube.Completer,
+		executor,
+		completer,
 		prompt.OptionTitle("jenny: interactive Jenkins CLI"),
 		prompt.OptionPrefix(">>> "),
 		prompt.OptionInputTextColor(prompt.Yellow),
